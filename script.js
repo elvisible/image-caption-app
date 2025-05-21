10b1e98ccfccd0dcedb0e17b943b8ceff1c45e41
elvisible <elvisible@hotmail.com>
2025-05-21 13:33:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function uploadToImgBB(base64Image) {
   const formData = new FormData();
   formData.append('image', base64Image.split(',')[1]);
 
-  const response = await fetch('https://api.imgbb.com/1/upload?key=YOUR_IMGBB_API_KEY', {
+  const response = await fetch('https://api.imgbb.com/1/upload?key=r8_TXipG1AgpvzXLUnJViWb1ADcZWpil5J1iL5dp', {
     method: 'POST',
     body: formData
   });
